093c55624e83240db8064c704ed2729e9bfb0afc
Muhammad Talha <talhaofficialwork@gmail.com>
2021-12-15 22:32:24 +0500

    Some Code Fixes and Formatting

--- functions/newLolly/newLolly.js
+++ functions/newLolly/newLolly.js
@@ -1,17 +1,16 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
-var faunadb = require('faunadb')
-q = faunadb.query
+const { ApolloServer, gql } = require('apollo-server-lambda');
+var faunadb = require('faunadb');
+q = faunadb.query;
 var dotenv = require('dotenv');
-dotenv.config()
-const shortid = require('shortid')
+dotenv.config();
+const shortid = require('shortid');
 
 const typeDefs = gql`
   type Query {
-   
     getLolly: [Lolly!]
     getlolly(lollyPath: String!):Lolly
-  
   }
+
   type Lolly {
     flavourTop: String!
     flavourMiddle: String!
@@ -20,37 +19,29 @@ const typeDefs = gql`
     message: String!
     senderName: String!
     lollyPath: String!
-    
   }
+
   type Mutation {
     createLolly(recipientName: String!,  message: String!, senderName: String!, flavourTop: String!, flavourMiddle: String!, flavourBottom:String!, lollyPath: String):Lolly
   }
-`
-
-
+`;
 
 const resolvers = {
   Query: {
-    getlolly: async (_,{lollyPath}) => {
-      
+    getlolly: async (_, { lollyPath }) => {
       try {
         const client = new faunadb.Client({ secret: process.env.ADMIN_SECRET });
-
         const result = await client.query(
           q.Get(q.Match(q.Index("lolly_by_path"), lollyPath))
-          )
-          
-               
-         return result.data
-        
-       
+        )
+        return result.data
       }
       catch (error) {
         console.log(error)
       }
     }
-
   },
+
   Mutation: {
     createLolly: async (_, { flavourTop, flavourMiddle, flavourBottom, recipientName, message, senderName }) => {
       try {
@@ -58,13 +49,12 @@ const resolvers = {
         const id = shortid.generate()
         const result = await client.query(
           q.Create(q.Collection('posts'), {
-            data: { flavourTop:flavourTop, flavourMiddle:flavourMiddle, flavourBottom:flavourBottom, recipientName:recipientName, message:message, senderName, lollyPath:id }
+            data: { flavourTop: flavourTop, flavourMiddle: flavourMiddle, flavourBottom: flavourBottom, recipientName: recipientName, message: message, senderName, lollyPath: id }
           })
         )
-        
-          return result.data
+        return result.data
       }
-      catch(error) {
+      catch (error) {
         console.log(error)
       }
     }
--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,5 +1,5 @@
 import fetch from 'cross-fetch';
-import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
     link: new HttpLink({
